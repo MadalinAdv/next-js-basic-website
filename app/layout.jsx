37b47161e5634e72c691e48c5878529bdfa04c77
Madalin <52881890+MadalinAdv@users.noreply.github.com>
2024-01-24 18:47:26 +0200

    finish design

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 export const metadata = {
     title: "Application",
     description: 'Discover & Share AI Prompts'
@@ -16,6 +19,7 @@ const RootLayout = ({children}) => {
             </div>
 
             <main className="app">
+                <Nav />
                 {children}
             </main>
         </body>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { signIn, singOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex-bbetween w-full mb-16 pt-3'>
+      <Link href="/" className='flex gap-2 flex-center'>
+        <Image
+          src="/assets/images/logo.svg"
+          alt='Promptopia Logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        ></Image>
+        <p className="logo_text">Promptopia</p>
+      </Link>
+    </nav>
   )
 }
 
