2360e2cf77140bbb9f59df3d7e37b7fcc54fc780
Madalin <52881890+MadalinAdv@users.noreply.github.com>
2024-01-24 18:27:12 +0200

    created components

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Feed from '@components/Feed';
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
         <p className='desc text-center'>
             Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
         </p>
+
+        {/* Feed*/}
     </section>
   )
 }
