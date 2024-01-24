32048b6ab8100af629eb4c25af270e49560bfa22
Madalin <52881890+MadalinAdv@users.noreply.github.com>
2024-01-24 18:20:37 +0200

    update version

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Application",
+    description: 'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className="main">
+                <div className="gradient">
+
+                </div>
+            </div>
+
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
